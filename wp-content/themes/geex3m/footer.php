cd73bdf7a80f7b6a5c2866085d9c22bc95e23ac7
TokyFy <tokyfydev@gmail.com>
2025-07-25 08:19:58 +0300

    Add new theme

---
+++ wp-content/themes/geex3m/footer.php
@@ -0,0 +1,18 @@
+
+<hr />
+<div id="footer">
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way, it's our only promotion or advertising. -->
+	<p>_______________________________________
+	<br />
+		<?php bloginfo('name'); ?> | <a href="http://wordpress.org/" title="WordPress">WP</a>
+		| RSS: <a href="<?php bloginfo('rss2_url'); ?>">entries</a> &amp; <a href="<?php bloginfo('comments_rss2_url'); ?>">comments</a>.
+		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</p>
+</div>
+</div>
+
+
+		<?php wp_footer(); ?>
+	
+</body>
+</html>
